2fdae9ee2a157202a53949670dd4f2d55541fdf0
chaeyoung <yimcha2y0ung@gmail.com>
2024-03-23 00:22:09 +0900

    [feat] CI/CD 구축

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -4,7 +4,7 @@ REPOSITORY=/home/ubuntu/build
 cd $REPOSITORY
 
 
-echo "1234" | sudo -S /usr/bin/yarn
-echo "1234" | sudo -S /usr/bin/yarn db:pull 
-echo "1234" | sudo -S /usr/bin/yarn generate 
-echo "1234" | sudo -S /usr/bin/pm2 start dist
+sudo -S /usr/bin/yarn
+sudo -S /usr/bin/yarn db:pull 
+sudo -S /usr/bin/yarn generate 
+sudo -S /usr/bin/pm2 start dist
